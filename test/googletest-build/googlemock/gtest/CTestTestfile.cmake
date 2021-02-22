f3ed9d2c591cc95df6f5d7c00abde66763bd8b2a
Javier <javier@kien.io>
2021-02-22 18:04:27 +0100

    Removed certain files and folders from tracking in Git

--- test/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/javier/repo/deploy_env_test/deploy_test_esp/test/googletest-src/googletest
-# Build directory: /home/javier/repo/deploy_env_test/deploy_test_esp/test/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
